0179b046f6668c0e88238ec65fa52e3a0712dfc9
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2024-02-13 09:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+    let split = str.split("");
+	let rev = split.reverse();
+	if(str.toLowerCase() == rev.toLowerCase()){
+		return true;
+	}
+	return false;
 }
 module.exports = palindrome
